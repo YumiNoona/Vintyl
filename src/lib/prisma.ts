85fd9d4274abac5fd34d2f827bd0cef3025aece8
Yuna <rushikeshingale2001@gmail.com>
2026-03-13 10:40:49 +0530

    Fix Prisma 7 init - pass empty options object to constructor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const client = globalForPrisma.prisma ?? new PrismaClient();
+export const client = globalForPrisma.prisma ?? new PrismaClient({});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = client;
